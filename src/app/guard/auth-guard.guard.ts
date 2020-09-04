30122bc45026539b0d837586a1fd11a1ec32f0c4
Nishant Shah <neonexus22@gmail.com>
2020-09-04 14:01:12 +0545

    auth added'

---
+++ src/app/guard/auth-guard.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuardGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    return !!localStorage.getItem('user');
+  }
+}
