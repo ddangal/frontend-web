b1c202fa0d5dea8512beb37b7acb5445a5944c53
zenita1 <lamichhanejenita@gmail.com>
2020-09-04 14:18:05 +0545

    signup completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: SignupComponent },
+  { path: 'home', component: DashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  apiUrl = environment.apiUrl;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  signup(data) {
+    let headers = new HttpHeaders();
+    headers.append('Content-Type' , 'multipart/form-data');
+    return this.http.post<any>(`${this.apiUrl}/api/accounts/signup/`, data, {headers: headers})
+      .pipe(map(user => {
+        return user;
+      }));
+  }
+
+}
